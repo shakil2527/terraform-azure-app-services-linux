64545eed738d3f4688d62d35c56ce428c72dd5b4
shakil2527 <65274532+shakil2527@users.noreply.github.com>
2021-03-05 19:13:52 +0530

    Update app-service-main.tf

--- app-service-main.tf
+++ app-service-main.tf
@@ -3,9 +3,8 @@
 ##############################
 
 # Create a Resource Group
-resource "azurerm_resource_group" "appservice-rg" {
-  name     = "kopi-${var.region}-${var.environment}-${var.app_name}-app-service-rg"
-  location = var.location
+data "azurerm_resource_group" "appservice-rg" {
+  name     = "myapp-${var.region}-${var.environment}-${var.app_name}-app-service-rg"
 
   tags = {
     description = var.description
@@ -16,9 +15,9 @@ resource "azurerm_resource_group" "appservice-rg" {
 
 # Create the App Service Plan
 resource "azurerm_app_service_plan" "service-plan" {
-  name                = "kopi-${var.region}-${var.environment}-${var.app_name}-service-plan"
-  location            = azurerm_resource_group.appservice-rg.location
-  resource_group_name = azurerm_resource_group.appservice-rg.name
+  name                = "myapp-${var.region}-${var.environment}-${var.app_name}-service-plan"
+  location            = var.location
+  resource_group_name = data.azurerm_resource_group.appservice-rg.name
   kind                = "Linux"
   reserved            = true
 
@@ -36,9 +35,9 @@ resource "azurerm_app_service_plan" "service-plan" {
 
 # Create the App Service
 resource "azurerm_app_service" "app-service" {
-  name                = "kopi-${var.region}-${var.environment}-${var.app_name}-app-service"
+  name                = "myapp-${var.region}-${var.environment}-${var.app_name}-app-service"
   location            = azurerm_resource_group.appservice-rg.location
-  resource_group_name = azurerm_resource_group.appservice-rg.name
+  resource_group_name = data.azurerm_resource_group.appservice-rg.name
   app_service_plan_id = azurerm_app_service_plan.service-plan.id
 
   site_config {
